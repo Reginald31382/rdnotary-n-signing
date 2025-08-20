1c7117becba0c51ae0a012e7f3234341c14eddd9
Reginald J Dixon <vassarjonez@yahoo.com>
2025-08-19 23:16:39 -0400

    updated more styling

--- components/About.tsx
+++ components/About.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { Radley } from "next/font/google";
+import { Lavishly_Yours } from "next/font/google";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import rdnotary from "@/public/rdnotary.png";
 import reggie1 from "@/public/reggie1.jpg";
 
 const radleyFont = Radley({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const lavishFont = Lavishly_Yours({
   subsets: ["latin"],
   weight: "400",
 });
@@ -38,6 +44,9 @@ export default function About() {
               fill
               className="rounded-2xl object-cover opacity-0 transition-opacity duration-500 group-hover:opacity-100"
             />
+            <h3 className="absolute top-10 left-1/2 transform -translate-x-1/2 text-black text-lg font-bold opacity-0 transition-opacity duration-500 group-hover:opacity-100 bg-black/40 px-3  rounded ">
+              Hello, I'm Reggie
+            </h3>
           </div>
         </div>
 
@@ -46,8 +55,10 @@ export default function About() {
         >
           <h2 className="text-4xl font-bold mb-4">Our Mission</h2>
           <p className="text-xl mb-4">
-            <span className="text-2xl">At, </span>
-            <span className="font-bold">R|D Notary & Signing </span>
+            <span className={`text-5xl font-bolder ${lavishFont.className}`}>
+              At {""}
+            </span>
+            <span className="font-bold">, R|D Notary & Signing </span>
             we provide reliable, professional, and timely notary services. Our
             goal is to make the notarization process simple and stress-free for
             individuals and businesses alike.
